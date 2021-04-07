61ec59ada6b6748f711f0b5cd613c3e222a57051
adachi-codelic <adachi-t@codelic.co>
2021-04-07 14:55:44 +0900

    完成目前

--- src/extension.ts
+++ src/extension.ts
@@ -1,155 +1,167 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import { stdout } from 'process'
 import * as vscode from 'vscode'
-import {
-  anyChar,
-  anyCharOf,
-  anyStringOf,
-  eof,
-  fail,
-  float,
-  letter,
-  newline,
-  noCharOf,
-  regexp,
-  string,
-  stringLen,
-  uniLetter,
-  whitespace
-} from 'parjs'
+import { eof, regexp, string } from 'parjs'
 import {
   between,
   many,
-  manySepBy,
   map,
   or,
   qthen,
   thenq,
   manyTill,
-  recover,
-  later,
-  stringify,
   then,
-  manyBetween,
-  flatten,
-  mapConst,
-  not
+  flatten
 } from 'parjs/combinators'
-import { visualizeTrace } from 'parjs/trace'
-import { ParjsSuccess } from 'parjs/internal/result'
+import * as pos from 'parjs/internal/parsers/position'
+import * as nl from 'parjs/internal/parsers/newline'
+import { pipe } from 'parjs/internal/combinators/combinator'
+import { space, spaces1 } from 'parjs/internal/parsers/char-types'
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-// function parenthesize (
-//   startSymbol: string,
-//   text: string,
-//   endSymbol: string
-// ): string {
-//   return startSymbol + text + endSymbol
-// }
 
 export function activate (context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('"vue-sass-peek" is now active!')
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
   class SassClassDefinitionProvider implements vscode.DefinitionProvider {
+    constructor () {}
     provideDefinition (
       document: vscode.TextDocument,
       position: vscode.Position,
       token: vscode.CancellationToken
     ) {
-      const workspace = vscode.workspace.getWorkspaceFolder(document.uri)
-      const root = workspace ? workspace.uri : document.uri
-      return new vscode.Location(
-        root.with({
-          path: root.path
-        }),
-        new vscode.Position(0, 1)
-      )
-    }
-  }
-  context.subscriptions.push(
-    vscode.languages.registerDefinitionProvider(
-      { scheme: 'file', language: 'vue' },
-      new SassClassDefinitionProvider()
-    )
-  )
-  let disposable = vscode.commands.registerCommand(
-    'vue-sass-peek.vue-sass-peek',
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      const text = vscode.window.activeTextEditor?.document.getText()
-      const sass = text?.match(
-        /<style lang="sass".*>(\r\n)*(\..*(\r\n)*)*<\/style>/
-      )
-      // console.log(sass)
-      vscode.window.showInformationMessage('Hello World from vue-sass-peek!')
-      // The module 'vscode' contains the VS Code extensibility API
-      // Import the module and reference it with the alias vscode in your code below
+      const targetText = document.getText()
 
-      class SassClass {
-        className: string
-        classContent: string
+      const classIdentifier = pos.position().pipe(then(regexp(/\S*/)))
 
-        constructor (className: string, classContent: string[]) {
-          this.className = className
-          this.classContent = classContent.reduce(
-            (acc, content) => acc + content,
-            ''
-          )
-          return this
-        }
-      }
+      const spaces = space().pipe(many())
 
-      const targetText = vscode.window.activeTextEditor?.document.getText()
-      // const sassText = (targetText ?? '').match(
-      //   /(?:<style.*>)((?<sassText>.*)|(?:\r\n))*(?:<\/style>)/
-      // )
-      // console.log(sassText)
-      const classIdentifier = regexp(/\S*/)
-      const sassClassName = string('.')
+      const sassClassName = spaces
+        .pipe(qthen(string('.')))
         .pipe(qthen(classIdentifier))
-        .pipe(thenq(regexp(/[^\S\r\n]*\r\n/)))
+        .pipe(thenq(spaces))
+        .pipe(thenq(nl.newline()))
+
       const styleOpenTag = regexp(/\s*<style.*>\s*/)
       const styleCloseTag = regexp(/\s*<\/style>\s*/)
-      const sassClassContentLine = regexp(/(?:[^\S\r\n]+.*)?(?:\r\n)/).pipe(
-        map(val => val[0])
-      )
-      const sassClassContent = sassClassContentLine.pipe(
-        // manyTill(sassClassName.pipe(or(styleCloseTag)))
-        many()
-      )
-      // const sassClassContent = sassClassContentLine.pipe(many())
-      const singleSassClass = sassClassName
+      const sassClassContentLine = regexp(/.*/)
+        .pipe(thenq(nl.newline()))
+        .pipe(map(val => val[0]))
+
+      const mediaQuery = spaces
+        .pipe(then(string('@media')))
+        .pipe(then(regexp(/.*/)))
+        .pipe(then(nl.newline()))
+        .pipe(map(val => val[0][0][0][0] + val[0][0][1] + val[0][1] + val[1]))
+
+      const sassClassContent = sassClassContentLine
+        .pipe(or(mediaQuery))
+        .pipe(many())
+
+      const sassComment = spaces
+        .pipe(then(string('//')))
+        .pipe(then(regexp(/.*/)))
+        .pipe(then(nl.newline()))
+
+      const singleSassClass = mediaQuery
+        .pipe(many())
+        .pipe(qthen(sassClassName))
         .pipe(then(sassClassContent))
-        .pipe(between(regexp(/(?:\r\n)*/)))
-      const sassComment = string('//').pipe(then(regexp(/.*\r\n/)))
+        .pipe(between(regexp(/(?:\r\n|\s*\/\/.*(?:\r\n))*/)))
+        .pipe(then(pos.position()))
       const sassClasses = sassComment
-        .pipe(or(/\r\n/))
+        .pipe(or(/(\r\n|\n)/))
         .pipe(many())
         .pipe(qthen(singleSassClass.pipe(many())))
       const sassParser = sassClasses.pipe(
         map(val => {
-          return val.map(values => {
-            return new SassClass(values[0][0], values[1])
-          })
+          return val.map(
+            values =>
+              new SassClass(
+                values[0][0][1][0],
+                values[0][1],
+                values[0][0][0],
+                values[1]
+              )
+          )
         })
       )
+      const otherPrevText = regexp(/(.*)/)
+        .pipe(then(nl.newline()))
+        .pipe(manyTill(styleOpenTag))
 
-      const otherPrevText = regexp(/(.*\r\n)/).pipe(manyTill(styleOpenTag))
-      const otherAfterText = regexp(/(.*\r\n)/).pipe(manyTill(eof()))
-      // const styleTag = sassClasses.pipe(between(styleOpenTag, styleCloseTag))
+      const otherAfterText = regexp(/(.*)/)
+        .pipe(then(nl.newline()))
+        .pipe(manyTill(eof()))
+
       const vueSassParser = otherPrevText
         .pipe(qthen(sassParser))
         .pipe(thenq(styleCloseTag))
         .pipe(thenq(otherAfterText))
 
       const sassClassesParjser = vueSassParser.parse(targetText ?? '')
+      console.log(sassClassesParjser)
+      const definedSassClasses = sassClassesParjser.isOk
+        ? sassClassesParjser.value
+        : []
+
+      // 選択されているテキストの取得
+      const editor = vscode.window.activeTextEditor
+      const selection = document.getWordRangeAtPosition(
+        editor?.selection.active ?? new vscode.Position(0, 0)
+      )
+      const selectedText = document.getText(selection)
+      console.log(definedSassClasses)
+      return definedSassClasses
+        .filter(sassClass => sassClass.className === selectedText)
+        .map(
+          targetSassClass =>
+            new vscode.Location(
+              document.uri,
+              new vscode.Range(
+                document.positionAt(targetSassClass.start),
+                document.positionAt(targetSassClass.end)
+              )
+            )
+        )
+    }
+  }
+
+  class SassClass {
+    className: string
+    classContent: string
+    start: number
+    end: number
+
+    constructor (
+      className: string,
+      classContent: string[],
+      start: number,
+      end: number
+    ) {
+      this.className = className
+      this.classContent = classContent.reduce(
+        (acc, content) => acc + content,
+        ''
+      )
+      this.start = start
+      this.end = end
+      return this
+    }
+  }
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand(
+    'vue-sass-peek.vue-sass-peek',
+    () => {
+      context.subscriptions.push(
+        vscode.languages.registerDefinitionProvider(
+          { scheme: 'file', language: 'vue' },
+          new SassClassDefinitionProvider()
+        )
+      )
     }
   )
   context.subscriptions.push(disposable)
